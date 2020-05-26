1ac7cbcb7c6f6485ac430dcb1898e7304b7ff2d1
Mohamed Halat <halatmohamed01@gmail.com>
2020-05-26 11:56:24 -0400

    Updated js and fixed async issue

--- ServerManager.js
+++ ServerManager.js
@@ -3,17 +3,21 @@ const eris = require('eris');
 const Compute = require('@google-cloud/compute');
 
 const sessionClient = new dialogflow.SessionsClient();
-const config = require('./config.json');
-const projectId = config.project_id;
-const sessionId = config.id;
-const languageCode = config.language;
+const projectId = "PLACEHOLDER";
+const sessionId = "0";
+const languageCode = "en";
 
 const compute = new Compute();
-const bot = new eris.Client(config.discord.token);
+
+const bot = new eris.CommandClient("PLACEHOLDER", {}, {
+    description: "A test bot made with Eris",
+    owner: "AhmedHalat",
+    prefix: "!"
+});
 
 // When the bot is connected and ready, log to console.
 bot.on('ready', () => {
-  console.log('Connected and ready. V0.8');
+  console.log('Connected and ready. V1.1.1');
 });
 
 async function detectIntent(projectId, sessionId, query, contexts, languageCode) {
@@ -38,6 +42,7 @@ async function detectIntent(projectId, sessionId, query, contexts, languageCode)
   }
 
   const responses = await sessionClient.detectIntent(request);
+  console.log("1res", responses)
   return responses[0];
 }
 
@@ -50,41 +55,201 @@ async function executeQueries(projectId, sessionId, query, languageCode) {
     console.log(`Sending Query: ${query}`);
     intentResponse = await detectIntent(projectId, sessionId, query, context,languageCode);
     console.log(`Fulfillment Text: ${intentResponse.queryResult.fulfillmentText}`);
-    responses = `${intentResponse.queryResult.fulfillmentText}`;
+    responses = intentResponse.queryResult.fulfillmentText || "There may have been an error";
     // Use the context from this response for next queries
     context = intentResponse.queryResult.outputContexts;
   } catch (error) {
     console.log(error);
   }
-
-  return responses
+  console.log("2res", responses)
+  return Promise.resolve(responses)
 }
 
 bot.on('messageCreate', async (msg) => {
-
-  try{
     var botWasMentioned = msg.mentions.find(mentionedUser => mentionedUser.id === bot.user.id,);
     if (botWasMentioned){
-      content = msg.content.toString().toLowerCase().replace("<@!685821709206421504>", "")
-      try {
-        var res = await executeQueries(projectId, sessionId, content, languageCode);
-        await msg.channel.createMessage(res);
-      } catch (e) {
-        console.log(e);
-        await msg.channel.createMessage("Sorry, I'm not sure I got that");
-      }
-
+      var author = `<@!${msg.author.id}>`;
+      content = msg.content.toString().toLowerCase().replace("<@!685821709206421504>", "");
+      var res = await Promise.resolve(executeQueries(projectId, sessionId, content, languageCode));
+      console.log("3res", res)
+      await msg.channel.createMessage(res);
     }
 
-    } catch (err) {
-      console.warn('Failed to respond to mention.');
-      console.error(err);
-      if (botWasMentioned) await msg.channel.createMessage('There seems to have been a problem.');
-    }
 });
 
 bot.on('error', err => {
   console.warn(err);
 });
+
+const vanillaCommand = bot.registerCommand("vanilla", (msg, args) => { // Make an echo command
+    if(args.length === 0) { // If the user just typed "!echo", say "Invalid input"
+      try {
+        var res = executeQueries(projectId, sessionId, "Status mc-server", languageCode)
+      } catch (e) {
+        return "ERROR"
+      }
+      return res;
+    }
+  },
+  {
+    description: "Make the bot say something",
+    fullDescription: "The bot will echo whatever is after the command label.",
+    reactionButtons: [ // Add reaction buttons to the command
+    {
+      emoji: "Ã¢â€žÂ¹Ã¯Â¸Â",
+      type: "edit",
+      response: (msg) => { // Reverse the message content
+        try {
+          var res = executeQueries(projectId, sessionId, "Status mc-server", languageCode)
+        } catch (e) {
+          return "ERROR"
+        }
+
+        return res;
+      }
+    },
+    {
+      emoji: "Ã¢â€“Â¶Ã¯Â¸Â",
+      type: "edit",
+      response: (msg) => { // Reverse the message content
+        try {
+          var res = executeQueries(projectId, sessionId, "start mc-server", languageCode)
+        } catch (e) {
+          return "FAILED TO START"
+        }
+        return res;
+      }
+    },
+    {
+      emoji: "Ã¢ÂÂ¹",
+      type: "edit", // Pick a new pong variation
+      response: (msg) => { // Reverse the message content
+        try {
+          var res = executeQueries(projectId, sessionId, "stop mc-server", languageCode)
+        } catch (e) {
+          return "FAILED TO STOP"
+        }
+        return res;
+      }
+    }
+  ]
+  }
+);
+
+vanillaCommand.registerSubcommand("start", (msg, args) => { // Make a reverse subcommand under echo
+    if(args.length === 0) { // If the user just typed "!echo reverse", say "Invalid input"
+      try {
+        var res = executeQueries(projectId, sessionId, "start mc-server", languageCode)
+      } catch (e) {
+        return "ERROR"
+      }
+      return res;
+    }
+  },
+  {
+    description: "Start the mc-server"
+  }
+);
+
+vanillaCommand.registerSubcommand("stop", (msg, args) => { // Make a reverse subcommand under echo
+    if(args.length === 0) { // If the user just typed "!echo reverse", say "Invalid input"
+      try {
+        var res = executeQueries(projectId, sessionId, "stop mc-server", languageCode)
+      } catch (e) {
+        return "ERROR"
+      }
+      return res;
+    }
+  },
+  {
+    description: "Stop the mc-server"
+  }
+);
+
+const windowsCommand = bot.registerCommand("windows", (msg, args) => { // Make an echo command
+    if(args.length === 0) { // If the user just typed "!echo", say "Invalid input"
+      try {
+        var res = executeQueries(projectId, sessionId, "Status windows", languageCode)
+      } catch (e) {
+        return "ERROR"
+      }
+      return res;
+    }
+  },
+  {
+    description: "Make the bot say something",
+    fullDescription: "The bot will echo whatever is after the command label.",
+    reactionButtons: [ // Add reaction buttons to the command
+      {
+        emoji: "Ã¢â€žÂ¹Ã¯Â¸Â",
+        type: "edit",
+        response: (msg) => { // Reverse the message content
+          try {
+            var res = executeQueries(projectId, sessionId, "Status windows", languageCode)
+          } catch (e) {
+            return "ERROR"
+          }
+
+          return res;
+        }
+      },
+      {
+        emoji: "Ã¢â€“Â¶Ã¯Â¸Â",
+        type: "edit",
+        response: (msg) => { // Reverse the message content
+          try {
+            var res = executeQueries(projectId, sessionId, "start windows", languageCode)
+          } catch (e) {
+            return "FAILED TO START"
+          }
+          return res;
+        }
+      },
+      {
+        emoji: "Ã¢ÂÂ¹",
+        type: "edit", // Pick a new pong variation
+        response: (msg) => { // Reverse the message content
+          try {
+            var res = executeQueries(projectId, sessionId, "stop windows", languageCode)
+          } catch (e) {
+            return "FAILED TO STOP"
+          }
+          return res;
+        }
+      }
+    ]
+  }
+);
+
+windowsCommand.registerSubcommand("start", (msg, args) => { // Make a reverse subcommand under echo
+  if(args.length === 0) { // If the user just typed "!echo reverse", say "Invalid input"
+    try {
+      var res = executeQueries(projectId, sessionId, "start windows", languageCode)
+    } catch (e) {
+      return "ERROR"
+    }
+    return res;
+  }
+  return "stat";
+},
+{
+  description: "Make the bot say something",
+  fullDescription: "The bot will echo whatever is after the command label."
+}
+);
+
+windowsCommand.registerSubcommand("stop", (msg, args) => { // Make a reverse subcommand under echo
+  if(args.length === 0) { // If the user just typed "!echo reverse", say "Invalid input"
+    try {
+      var res = executeQueries(projectId, sessionId, "stop windows", languageCode)
+    } catch (e) {
+      return "ERROR"
+    }
+    return res;
+  }
+});
+
+bot.registerCommandAlias("mc", "vanilla");
+bot.registerCommandAlias("pc", "windows");
 
 bot.connect();
